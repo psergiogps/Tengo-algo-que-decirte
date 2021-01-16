3717c6f65af3cfd851cae4a959a9c506dcbf8c4a
Sergius <psergiogps@gmail.com>
2021-01-16 18:17:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate
 
 VoiceRSS.speech({
   key: '87db78e5bda0410287866b879f3bf413',
-  src: 'Sí, sí... lo que tú digas',
+  src: 'Kiko, cabrón, cómeme el rabo',
   hl: 'es-es',
   v: 'Camila',
   r: 0, 
