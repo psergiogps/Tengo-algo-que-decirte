9a35b94112cba115be6a4964e7b0ad062b0757c0
Sergius <psergiogps@gmail.com>
2021-01-20 16:06:47 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate
 
 VoiceRSS.speech({
   key: '87db78e5bda0410287866b879f3bf413',
-  src: 'Alberto, vamos a tomar una puta birra y comer algo',
+  src: 'Algo',
   hl: 'es-es',
   v: 'Camila',
   r: 0, 
