5471c9807587c692eb1c5e658f76c1a2f15df149
Sergius <psergiogps@gmail.com>
2021-01-20 13:37:19 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate
 
 VoiceRSS.speech({
   key: '87db78e5bda0410287866b879f3bf413',
-  src: 'Algo',
+  src: 'Alberto, vamos a buscar alguna puta terraza para comer algo',
   hl: 'es-es',
   v: 'Camila',
   r: 0, 
